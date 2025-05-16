97d07374b8110ab743a7930ebcc86a3cffa921db
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-16 18:12:16 -0400

    Logging set up and Constants started

--- ChargedUp/Bucky/src/main/java/frc/robot/Constants.java
+++ ChargedUp/Bucky/src/main/java/frc/robot/Constants.java
@@ -1,8 +1,24 @@
 package frc.robot;
 
-public class Constants {
+import edu.wpi.first.wpilibj.RobotBase;
 
-    public final class DriveTrainConstants{
+public final class Constants {
+    public static final Mode simMode = Mode.SIM;
+    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+    public static enum Mode {
+        /** Running on a real robot. */
+        REAL,
+
+        /** Running a physics simulator. */
+        SIM,
+
+        /** Replaying from a log file. */
+        REPLAY
+    }
+
+    public final static class DriveTrainConstants{
+        /*  Motor IDs */
         public static final int RIGHT_DRIVE_1 = 1;
         public static final int RIGHT_DRIVE_2 = 3;
         public static final int RIGHT_DRIVE_3 = 5;

63064460ec9874cd5f61260fda3edc9d28b1216c
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-16 16:33:31 -0400

    Constants File

---
+++ ChargedUp/Bucky/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Constants {
+
+    public final class DriveTrainConstants{
+        public static final int RIGHT_DRIVE_1 = 1;
+        public static final int RIGHT_DRIVE_2 = 3;
+        public static final int RIGHT_DRIVE_3 = 5;
+
+        public static final int LEFT_DRIVE_1 = 2;
+        public static final int LEFT_DRIVE_2 = 4;
+        public static final int LEFT_DRIVE_3 = 6;
+
+    }
+}

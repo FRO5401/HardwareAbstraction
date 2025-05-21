59b2e13be033c97a85fd75bbf3575b53fc86a51d
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-21 19:43:33 -0400

    Setting up advantage kit and Constants file

--- ChargedUp/Bucky/src/main/java/frc/robot/Robot.java
+++ ChargedUp/Bucky/src/main/java/frc/robot/Robot.java
@@ -4,17 +4,52 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+    Logger.recordMetadata("ProjectName", "Bucky");
+
+    String logPath;
+    switch (Constants.currentMode) {
+      case REAL:
+        logPath = "logs";
+        // Running on a real robot, log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new WPILOGWriter(logPath));
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case SIM:
+        logPath = "logs";
+        // Running a physics simulator, log to NT
+        Logger.addDataReceiver(new WPILOGWriter(logPath));
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+        // Replaying a log, set up replay source
+        setUseTiming(false); // Run as fast as possible
+        logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    Logger.start();
   }
 
   @Override

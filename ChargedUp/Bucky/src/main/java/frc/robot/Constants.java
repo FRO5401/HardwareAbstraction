8cad6631553026cac36c8bfc3595eeb25f96fb9e
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-05-21 19:56:19 -0400

    Spacing

--- ChargedUp/Bucky/src/main/java/frc/robot/Constants.java
+++ ChargedUp/Bucky/src/main/java/frc/robot/Constants.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public class Constants {
+    
     public static final Mode simMode = Mode.SIM;
     public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 

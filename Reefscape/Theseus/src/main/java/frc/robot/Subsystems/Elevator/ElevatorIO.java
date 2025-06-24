1a15a2c9bf2652064685d9ab0612cb592549dc25
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-06-24 18:49:08 -0400

    Elevator Input file

---
+++ Reefscape/Theseus/src/main/java/frc/robot/Subsystems/Elevator/ElevatorIO.java
@@ -0,0 +1,24 @@
+package frc.robot.Subsystems.Elevator;
+
+public interface ElevatorIO {
+    public default void updateInputs(ElevatorIOInputs inputs){};
+
+    public class ElevatorIOInputs{
+
+        public double position = 0;
+
+        public double velocity = 0;
+
+        public double temperature = 0;
+
+        public double voltage;
+        
+        public double current = 0;
+    }
+
+    public default void setPosition(double position){}
+
+    public default void stop(){}
+
+    public default void setVoltage(double voltage){}
+}

77f611eb6b6f0ed82887cc7987bf8c5c86bd6c4d
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-06-25 16:38:42 -0400

    Hardware file commit

--- Reefscape/Theseus/src/main/java/frc/robot/Subsystems/Elevator/ElevatorIOTalonFX.java
+++ Reefscape/Theseus/src/main/java/frc/robot/Subsystems/Elevator/ElevatorIOTalonFX.java
@@ -1,5 +1,43 @@
 package frc.robot.Subsystems.Elevator;
 
-public class ElevatorIOTalonFX implements ElevatorIO{
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
+import frc.robot.Subsystems.Elevator.ElevatorConstants.Config;
+
+public class ElevatorIOTalonFX implements ElevatorIO{
+  private final TalonFX elevatorMotor = new TalonFX(ElevatorConstants.elevatorID);
+  private final TalonFXConfiguration config = Config.ELEVATOR_CONFIG;
+  private final PositionVoltage positionPID = new PositionVoltage(0).withSlot(0);
+
+  public ElevatorIOTalonFX(){
+    elevatorMotor.setPosition(0);
+    elevatorMotor.getConfigurator().apply(config);
+  }
+
+  public void updateInputs(ElevatorIOInputs inputs){
+    inputs.position = elevatorMotor.getPosition().refresh().getValueAsDouble();
+    inputs.voltage = elevatorMotor.getMotorVoltage().refresh().getValueAsDouble();
+    inputs.velocity = elevatorMotor.getVelocity().refresh().getValueAsDouble();
+    inputs.current = elevatorMotor.getStatorCurrent().refresh().getValueAsDouble();
+    inputs.temperature = elevatorMotor.getDeviceTemp().refresh().getValueAsDouble();
+  }
+
+  public void setVoltage(double voltage){
+    elevatorMotor.setVoltage(voltage);
+  }
+
+  public void setPosition(double position){
+    if(position > ElevatorConstants.MAX_EXTENSION){
+      position = ElevatorConstants.MAX_EXTENSION;
+    } else if(position>ElevatorConstants.MIN_EXTENSION){
+      position = ElevatorConstants.MIN_EXTENSION;
+    }
+    elevatorMotor.setControl(positionPID.withPosition(position));
+  }
+
+  public void stop(){
+    elevatorMotor.set(0);
+  }
 }
